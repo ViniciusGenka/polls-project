699f562f5293c3a6a66f226b7c98edeae26a7bde
ViniciusGenka <vini.genka@hotmail.com>
2022-09-05 00:43:16 -0300

    refactor: create bad request helper function and implement it in signUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,18 +1,13 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingFieldError } from '../errors/missingFieldError'
+import { badRequest } from '../helpers/httpHelper'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingFieldError('name')
-      }
+      return badRequest(new MissingFieldError('name'))
     }
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingFieldError('email')
-      }
+      return badRequest(new MissingFieldError('email'))
     }
   }
 }
