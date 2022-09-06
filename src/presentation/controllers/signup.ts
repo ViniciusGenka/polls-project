69ce84f3dc893297f819379254c618e0af7a3543
ViniciusGenka <vini.genka@hotmail.com>
2022-09-05 22:51:22 -0300

    feat: ensure SignUpController returns an error if an invalid email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,14 +1,26 @@
 import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingFieldError } from '../errors/missingFieldError'
+import { InvalidFieldError } from '../errors/invalidFieldError'
 import { badRequest } from '../helpers/httpHelper'
+import { EmailValidator } from '../protocols/emailValidator'
+
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingFieldError(field))
       }
+    }
+    const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!emailIsValid) {
+      return badRequest(new InvalidFieldError('email'))
     }
   }
 }
